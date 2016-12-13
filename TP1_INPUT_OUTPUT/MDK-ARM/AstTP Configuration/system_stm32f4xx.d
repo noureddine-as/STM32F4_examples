bf445db041f29ebd4521f21bc7c1192917f373e8
Noureddine Ait Said <noureddine.asni@gmail.com>
2016-12-13 00:18:35 +0000

    1st Example using STM32CubeMX and ARM Keil v5

---
+++ TP1_INPUT_OUTPUT/MDK-ARM/AstTP Configuration/system_stm32f4xx.d
@@ -0,0 +1,9 @@
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Include/core_cm4.h
+asttp configuration/\system_stm32f4xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Include/core_cmSimd.h
+asttp configuration/\system_stm32f4xx.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
